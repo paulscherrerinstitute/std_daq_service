cc0fa4fcced765e7f49de950e1beb2ca6f8e5565
Andrej Babic <andrej.babic@psi.ch>
2021-05-27 13:57:43 +0200

    Add BrokerListener implementation

--- sf_daq_service/common/broker_listener.py
+++ sf_daq_service/common/broker_listener.py
@@ -1,3 +1,14 @@
+import json
+import logging
+import uuid
+from functools import partial
+
+from pika import BlockingConnection, ConnectionParameters, BasicProperties
+
+from sf_daq_service.common import broker_config
+
+_logger = logging.getLogger("BrokerListener")
+
 
 class BrokerListener(object):
     def __init__(self, broker_url, tag, on_message_function):
@@ -5,11 +16,56 @@ class BrokerListener(object):
         self.tag = tag
         self.on_message = on_message_function
 
+        self.status_queue_name = str(uuid.uuid4())
+        _logger.debug(f"Status queue name: {self.status_queue_name}")
+
+        self.connection = None
+        self.channel = None
+
     def start(self):
+        self.connection = BlockingConnection(ConnectionParameters(self.broker_url))
+        self.channel = self.connection.channel()
+
+        self.channel.exchange_declare(exchange=broker_config.STATUS_EXCHANGE,
+                                      exchange_type=broker_config.STATUS_EXCHANGE_TYPE)
+        self.channel.exchange_declare(exchange=broker_config.REQUEST_EXCHANGE,
+                                      exchange_type=broker_config.REQUEST_EXCHANGE_TYPE)
+
+        self.channel.queue_declare(queue=self.status_queue_name, exclusive=True, auto_delete=True)
+        self.channel.queue_bind(queue=self.status_queue_name,
+                                exchange=broker_config.STATUS_EXCHANGE,
+                                routing_key=self.tag)
+
+        self.channel.basic_consume(self.status_queue_name, self._on_broker_message, auto_ack=True)
+
         try:
-            pass
+            self.channel.start_consuming()
         except KeyboardInterrupt:
-            self.stop()
+            self.channel.stop_consuming()
 
     def stop(self):
-        pass
+        self.connection.add_callback_threadsafe(self.channel.stop_consuming)
+
+    def _on_broker_message(self, channel, method_frame, header_frame, body):
+        try:
+            request = json.loads(body.decode())
+            self.on_message(header_frame, request)
+
+        except Exception as e:
+            _logger.exception("Error in broker listener.")
+
+    def send_request(self, tag, header, message):
+
+        _logger.info(f'Sending request to tag {tag} with header {header} and message {message}')
+
+        body = json.dumps(message).encode()
+
+        send_request_f = partial(
+            self.channel.basic_publish,
+            broker_config.REQUEST_EXCHANGE,
+            tag,
+            body,
+            BasicProperties(headers=header)
+        )
+
+        self.connection.add_callback_threadsafe(send_request_f)
