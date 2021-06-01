b88e72cd56011321ee3df4b8e8494ea35f267c53
Andrej Babic <andrej.babic@ringlspil.com>
2021-06-01 09:17:07 +0200

    Refactor REST write interface
    Allow the request building code to be shared between the
    sync and async requests.

--- sf_daq_service/rest/request_factory.py
+++ sf_daq_service/rest/request_factory.py
@@ -1,3 +1,8 @@
 def build_write_request(output_file, n_images, sources):
     # TODO: Add timestamp of request building
     return None
+
+
+def build_broker_response(response):
+    # TODO: Convert the response into something the user can read.
+    return response
--- sf_daq_service/rest/start.py
+++ sf_daq_service/rest/start.py
@@ -5,14 +5,29 @@ from flask import Flask, request, jsonify
 
 from sf_daq_service.common import broker_config
 from sf_daq_service.common.broker_client import BrokerClient
-from sf_daq_service.rest.request_factory import build_write_request
+from sf_daq_service.rest.request_factory import build_write_request, build_broker_response
 from sf_daq_service.rest.status_aggregator import StatusAggregator
 
 _logger = logging.getLogger("RestProxyService")
 
 
-def on_status_message():
-    pass
+def extract_write_request(request_data):
+
+    if 'output_file' not in request_data:
+        raise RuntimeError('Mandatory field "output_file" missing.')
+    output_file = request_data['output_file']
+
+    if 'n_images' not in request_data:
+        raise RuntimeError('Mandatory field "n_images" missing.')
+    n_images = request_data['n_images']
+
+    if 'sources' not in request_data:
+        raise RuntimeError('Mandatory field "sources" missing.')
+    sources = request_data['sources']
+    if isinstance(request_data['sources'], list):
+        raise RuntimeError('Field "sources" must be a list.')
+
+    return build_write_request(output_file=output_file, n_images=n_images, sources=sources)
 
 
 def start_rest_api(service_name, broker_url, tag):
@@ -22,31 +37,16 @@ def start_rest_api(service_name, broker_url, tag):
     broker_client = BrokerClient(broker_url, tag,
                                  on_status_message_function=status_aggregator.on_broker_message)
 
-    @app.route("/write", methods=['POST'])
-    def write_request():
-        request_data = request.json
+    @app.route("/write_sync", methods=['POST'])
+    def write_sync_request():
+        header, body = extract_write_request(request.json)
 
-        if 'output_file' not in request_data:
-            raise RuntimeError('Mandatory field "output_file" missing.')
-        output_file = request_data['output_file']
-
-        if 'n_images' not in request_data:
-            raise RuntimeError('Mandatory field "n_images" missing.')
-        n_images = request_data['n_images']
-
-        if 'sources' not in request_data:
-            raise RuntimeError('Mandatory field "sources" missing.')
-        sources = request_data['sources']
-        if isinstance(request_data['sources'], list):
-            raise RuntimeError('Field "sources" must be a list.')
-
-        header, body = build_write_request(output_file=output_file, n_images=n_images, sources=sources)
-
-        request_id = broker_client.send_request(tag, body)
+        request_id = broker_client.send_request(tag, body, header)
         broker_response = status_aggregator.wait_for_response(request_id)
 
         # TODO: Convert broker_response to client response.
-        response = {"broker": broker_response}
+        response = {"request_id": request_id,
+                    'response': build_broker_response(response=broker_response)}
 
         return jsonify(response)
 
