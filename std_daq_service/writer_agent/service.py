8e0c3b156bf21dec3ccf74e0f6fbac506fee1410
Andrej Babic <andrej.babic@psi.ch>
2021-06-16 17:59:11 +0200

    Add service interruption logic

--- std_daq_service/writer_agent/service.py
+++ std_daq_service/writer_agent/service.py
@@ -1,7 +1,9 @@
+from logging import getLogger
 from threading import Event
 
 from std_daq_service.common.protocol import ImageMetadata
-from std_daq_service.writer_agent.start import _logger
+
+_logger = getLogger("RequestWriterService")
 
 
 class RequestWriterService(object):
@@ -15,22 +17,36 @@ class RequestWriterService(object):
 
         self.i_image = None
 
+    def _interrupt(self, image_meta):
+        if self.interrupt_request != self.request_id:
+            self.interrupt_request = None
+            return
+
+        self._complete_request()
+
+        return {
+            "output_file": self.request["output_file"],
+            "i_image": self.request["n_images"]-1,
+            "n_images": self.request["n_images"],
+            "image_metadata": image_meta.as_dict()
+        }
+
     def on_stream_message(self, recv_bytes: bytes):
         if self.request is None:
             return None
 
-        if self.interrupt_request is not None:
-            if self.request_id == self.interrupt_request:
-                self._complete_request()
-            else:
-                self.interrupt_request = None
-
         image_meta = ImageMetadata.from_buffer_copy(recv_bytes)
+
+        if self.interrupt_request is not None:
+            is_interrupted = self._interrupt(image_meta=image_meta)
+
+            if is_interrupted:
+                return is_interrupted
 
         if self.i_image is None:
             self.i_image = 0
             self.request_result = {
-                'start_pulse_id': image_meta.pulse_id,
+                'start_pulse_id': image_meta.id,
                 'n_images': self.request['n_images'],
                 'output_file': self.request["output_file"]
             }
@@ -43,7 +59,7 @@ class RequestWriterService(object):
         }
 
         if self.i_image + 1 == self.request["n_images"]:
-            self.request_result["end_pulse_id"] = image_meta.pulse_id
+            self.request_result["end_pulse_id"] = image_meta.id
             self._complete_request()
         else:
             self.i_image += 1
@@ -52,9 +68,10 @@ class RequestWriterService(object):
 
     def _complete_request(self):
         self.request = None
+        self.request_id = None
         self.i_image = None
-        self.request_completed.set()
         self.interrupt_request = None
+        self.request_completed.set()
 
     def on_broker_message(self, request_id, request):
         self._set_request_and_wait(request_id, request)
@@ -65,6 +82,8 @@ class RequestWriterService(object):
     def _set_request_and_wait(self, request_id, request):
         _logger.info(f"Starting to work on request_id {request_id}, {request}")
         self.request_result = None
+        self.interrupt_request = None
+        self.request_id = request_id
         self.request = request
         self.request_completed.wait()
 
