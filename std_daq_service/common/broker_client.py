0c4e11a0a52fba87ca319d2e4edaaca3e0c6a763
Andrej Babic <andrej.babic@psi.ch>
2021-06-21 13:36:30 +0200

    Update broker client with update status logic

--- std_daq_service/common/broker_client.py
+++ std_daq_service/common/broker_client.py
@@ -1,76 +1,93 @@
 import json
-import logging
 import uuid
 from functools import partial
+from logging import getLogger
+from threading import Thread
 
 from pika import BlockingConnection, ConnectionParameters, BasicProperties
 
-from std_daq_service.common import broker_config
+TEST_BROKER_URL = '127.0.0.1'
 
-_logger = logging.getLogger("BrokerClient")
+STATUS_EXCHANGE = 'status'
+STATUS_EXCHANGE_TYPE = 'fanout'
+
+REQUEST_EXCHANGE = 'request'
+REQUEST_EXCHANGE_TYPE = 'topic'
+
+KILL_EXCHANGE = 'kill'
+KILL_EXCHANGE_TYPE = 'topic'
+
+ACTION_REQUEST_START = "request_start"
+ACTION_REQUEST_SUCCESS = "request_success"
+ACTION_REQUEST_FAIL = "request_fail"
+
+_logger = getLogger("RabbitMQConnection")
 
 
 class BrokerClient(object):
-    def __init__(self, broker_url, status_tag, on_status_message_function):
-        self.broker_url = broker_url
-        self.status_tag = status_tag
-        self.on_message = on_status_message_function
+    def __init__(self, broker_url, tag, status_callback=None, request_callback=None, kill_callback=None):
 
-        self.status_queue_name = str(uuid.uuid4())
-        _logger.debug(f"Status queue name: {self.status_queue_name}")
-
-        self.connection = None
-        self.channel = None
-
-    def start(self):
-        self.connection = BlockingConnection(ConnectionParameters(self.broker_url))
+        self.connection = BlockingConnection(ConnectionParameters(broker_url))
         self.channel = self.connection.channel()
-
-        self.channel.exchange_declare(exchange=broker_config.STATUS_EXCHANGE,
-                                      exchange_type=broker_config.STATUS_EXCHANGE_TYPE)
-        self.channel.exchange_declare(exchange=broker_config.REQUEST_EXCHANGE,
-                                      exchange_type=broker_config.REQUEST_EXCHANGE_TYPE)
-        self.channel.exchange_declare(exchange=broker_config.KILL_EXCHANGE,
-                                      exchange_type=broker_config.KILL_EXCHANGE_TYPE)
-
-        self.channel.queue_declare(queue=self.status_queue_name, exclusive=True, auto_delete=True)
-        self.channel.queue_bind(queue=self.status_queue_name,
-                                exchange=broker_config.STATUS_EXCHANGE,
-                                routing_key=self.status_tag)
-
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(self.status_queue_name, self._on_broker_message, auto_ack=True)
+        self.tag = tag
 
-        try:
-            self.channel.start_consuming()
-        except KeyboardInterrupt:
-            self.channel.stop_consuming()
+        self.channel.exchange_declare(exchange=REQUEST_EXCHANGE, exchange_type=REQUEST_EXCHANGE_TYPE)
+        self.channel.exchange_declare(exchange=STATUS_EXCHANGE, exchange_type=STATUS_EXCHANGE_TYPE)
+        self.channel.exchange_declare(exchange=KILL_EXCHANGE, exchange_type=KILL_EXCHANGE_TYPE)
+
+        if status_callback is not None:
+            self._bind_queue(STATUS_EXCHANGE, tag, status_callback, True)
+
+        if request_callback is not None:
+            self._bind_queue(REQUEST_EXCHANGE, tag, request_callback, False)
+
+        if kill_callback is not None:
+            self._bind_queue(KILL_EXCHANGE, tag, kill_callback, True)
+
+        def start_consuming():
+            try:
+                self.channel.start_consuming()
+            except KeyboardInterrupt:
+                self.channel.stop_consuming()
+
+        self.thread = Thread(target=start_consuming)
+        self.thread.start()
+
+    def __del__(self):
+        self.stop()
+
+    def _bind_queue(self, exchange, tag, callback, auto_ack):
+        queue = str(uuid.uuid4())
+
+        _logger.info(f"Binding queue {queue} to exchange {exchange} with tag {tag}.")
+
+        self.channel.queue_declare(queue, auto_delete=True, exclusive=True)
+        self.channel.queue_bind(queue=queue,
+                                exchange=exchange,
+                                routing_key=tag)
+
+        self.channel.basic_consume(queue, callback, auto_ack=auto_ack)
+
+    def block(self):
+        self.thread.join()
 
     def stop(self):
+        _logger.info("Stopping connection.")
+
         self.connection.add_callback_threadsafe(self.channel.stop_consuming)
+        self.thread.join()
 
-    def _on_broker_message(self, channel, method_frame, header_frame, body):
-        try:
-            request = json.loads(body.decode())
-            self.on_message(header_frame.correlation_id, header_frame.headers, request)
-
-        except Exception as e:
-            _logger.exception("Error in broker listener.")
-
-    def send_request(self, tag, message, header=None):
-
+    def send_request(self, message, header=None):
         header = header or {}
 
-        _logger.info(f'Sending request to tag {tag} with header {header} and message {message}')
+        _logger.info(f'Sending request to tag {self.tag} with header {header} and message {message}')
 
         body = json.dumps(message).encode()
         request_id = str(uuid.uuid4())
 
         send_request_f = partial(
-            self.channel.basic_publish,
-            broker_config.REQUEST_EXCHANGE,
-            tag,
-            body,
+            self.channel.basic_publish, REQUEST_EXCHANGE, self.tag, body,
             BasicProperties(headers=header,
                             correlation_id=request_id)
         )
@@ -79,16 +96,24 @@ class BrokerClient(object):
 
         return request_id
 
-    def kill_request(self, tag, request_id):
-        _logger.info(f'Sending kill request to tag {tag} with request_id {request_id}.')
+    def kill_request(self, request_id):
+        _logger.info(f'Sending kill request to tag {self.tag} with request_id {request_id}.')
 
         kill_request_f = partial(
-            self.channel.basic_publish,
-            broker_config.KILL_EXCHANGE,
-            tag,
-            "",
+            self.channel.basic_publish, KILL_EXCHANGE, self.tag, "",
             BasicProperties(headers={},
                             correlation_id=request_id)
         )
 
         self.connection.add_callback_threadsafe(kill_request_f)
+
+    def update_status(self, request_id, message, header):
+
+        _logger.info(f"Updating worker status: {header}")
+        properties = BasicProperties(headers=header, correlation_id=request_id)
+
+        update_status_f = partial(
+            self.request_channel.basic_publish,
+            STATUS_EXCHANGE, self.tag, message, properties)
+
+        self.connection.add_callback_threadsafe(update_status_f)
