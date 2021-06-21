b13cf5a679159637cc6c302cbd2ef79338784853
Andrej Babic <andrej.babic@psi.ch>
2021-06-21 18:13:35 +0200

    Broker service rework

--- std_daq_service/broker_service.py
+++ std_daq_service/broker_service.py
@@ -1,66 +1,30 @@
 import json
-import uuid
-from functools import partial
 from logging import getLogger
 from threading import Thread
 
-from pika import BlockingConnection, ConnectionParameters, BasicProperties
+from pika import BasicProperties
 
-from std_daq_service.common.broker_utils import bind_queue_to_exchange
-
-TEST_BROKER_URL = '127.0.0.1'
-
-STATUS_EXCHANGE = 'status'
-STATUS_EXCHANGE_TYPE = 'fanout'
-
-REQUEST_EXCHANGE = 'request'
-REQUEST_EXCHANGE_TYPE = 'topic'
-
-KILL_EXCHANGE = 'kill'
-KILL_EXCHANGE_TYPE = 'topic'
+from std_daq_service.common.broker import BrokerClientBase, STATUS_EXCHANGE, REQUEST_EXCHANGE, KILL_EXCHANGE
 
 ACTION_REQUEST_START = "request_start"
 ACTION_REQUEST_SUCCESS = "request_success"
 ACTION_REQUEST_FAIL = "request_fail"
 
-_logger = getLogger("RabbitMQConnection")
+_logger = getLogger("BrokerService")
 
 
-class BrokerService(object):
+class BrokerService(BrokerClientBase):
     def __init__(self, broker_url, tag, service_name):
-        self.tag = tag
+        super().__init__(broker_url=broker_url, tag=tag)
+
         self.service_name = service_name
+        _logger.info(f"Service {service_name} starting.")
 
-        self.connection = BlockingConnection(ConnectionParameters(broker_url))
-        self.channel = self.connection.channel()
-        self.channel.basic_qos(prefetch_count=1)
-
-        self.channel.exchange_declare(exchange=REQUEST_EXCHANGE, exchange_type=REQUEST_EXCHANGE_TYPE)
-        self.channel.exchange_declare(exchange=STATUS_EXCHANGE, exchange_type=STATUS_EXCHANGE_TYPE)
-        self.channel.exchange_declare(exchange=KILL_EXCHANGE, exchange_type=KILL_EXCHANGE_TYPE)
-
-        bind_queue_to_exchange(self.channel, REQUEST_EXCHANGE, tag, self._request_callback, False)
-        bind_queue_to_exchange(self.channel, KILL_EXCHANGE, tag, self._kill_callback, True)
+        self.bind_queue(REQUEST_EXCHANGE, self.tag, self._request_callback, False)
+        self.bind_queue(KILL_EXCHANGE, self.tag, self._kill_callback, True)
 
         self.user_request_callback = None
         self.user_kill_callback = None
-        self.user_status_callback = None
-
-        self.request_id_cache = {}
-
-        _logger.info(f"Starting with tag {tag} and service_name {service_name} on broker_url {broker_url}.")
-
-        def start_consuming():
-            try:
-                self.channel.start_consuming()
-            except KeyboardInterrupt:
-                self.channel.stop_consuming()
-
-        self.thread = Thread(target=start_consuming)
-        self.thread.start()
-
-    def __del__(self):
-        self.stop()
 
     def _request_callback(self, channel, method_frame, header_frame, body):
 
@@ -91,6 +55,7 @@ class BrokerService(object):
                         'source': self.client_name,
                         'message': result
                     }))
+
                 self.channel.basic_ack(delivery_tag=delivery_tag)
 
             except Exception as e:
@@ -115,12 +80,3 @@ class BrokerService(object):
         _logger.info(f"Received kill for request_id {request_id}.")
 
         self.user_kill_callback(request_id)
-
-    def block(self):
-        self.thread.join()
-
-    def stop(self):
-        _logger.info("Stopping connection.")
-
-        self.connection.add_callback_threadsafe(self.channel.stop_consuming)
-        self.thread.join()
