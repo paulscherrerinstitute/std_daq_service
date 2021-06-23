b2aea3171e1c0efe98b31c69ccb6067a42edc5c6
Andrej Babic <andrej.babic@psi.ch>
2021-06-23 12:02:04 +0200

    Add multiple runs test

--- tests/test_request_write_service.py
+++ tests/test_request_write_service.py
@@ -130,3 +130,86 @@ class TestRequestWriteService(unittest.TestCase):
 
         listener.stop()
         client.stop()
+
+    def test_multiple_requests(self):
+        tag = 'multi_service'
+        n_images = 5
+
+        def on_status_message(request_id, request, header):
+            nonlocal last_header
+            last_header = header
+        last_header = None
+
+        sender, receiver, listener, service, client = self._setup_service(tag, tag, on_status_message)
+        sleep(0.1)
+
+        # Send write request.
+        first_request_id = client.send_request({
+            "n_images": n_images,
+            "output_file": "/test/output.h5"
+        })
+        sleep(0.1)
+        self.assertEqual(last_header['action'], ACTION_REQUEST_START)
+
+        # Send images into the transceiver.
+        for pulse_id in range(n_images):
+            sender.send(ImageMetadata(1, pulse_id, 1024, 512, 1, 1))
+        sleep(0.1)
+
+        # Receive the write request on the other side.
+        for pulse_id in range(n_images):
+            write_message = receiver.recv_json()
+            self.assertEqual(write_message['i_image'], pulse_id)
+
+        self.assertEqual(last_header['action'], ACTION_REQUEST_SUCCESS)
+        last_header = None
+
+        # Send images that will be ignored in between.
+        for pulse_id in range(n_images, n_images*2):
+            sender.send(ImageMetadata(1, pulse_id, 1024, 512, 1, 1))
+        sleep(0.1)
+
+        # Send another request.
+        second_request_id = client.send_request({
+            "n_images": n_images,
+            "output_file": "/test/output.h5"
+        })
+        sleep(0.1)
+        self.assertEqual(last_header['action'], ACTION_REQUEST_START)
+
+        # But with 1 message in the stream less than we expected.
+        for pulse_id in range(n_images*2, (n_images*3)-1):
+            sender.send(ImageMetadata(1, pulse_id, 1024, 512, 1, 1))
+        sleep(0.1)
+
+        # Confirm the status did not already change.
+        self.assertEqual(last_header['action'], ACTION_REQUEST_START)
+
+        # Check if we received the partial write stream.
+        for pulse_id in range(n_images*2, (n_images*3)-1):
+            write_message = receiver.recv_json()
+            # self.assertEqual(write_message['i_image'], pulse_id)
+
+        # The first request id should not kill the request anymore.
+        client.kill_request(first_request_id)
+        sleep(0.2)
+
+        # Verify the kill with the old request id did nothing.
+        self.assertEqual(last_header['action'], ACTION_REQUEST_START)
+
+        # This is the correct request id to kill
+        client.kill_request(second_request_id)
+        sleep(0.2)
+
+        # Verify the kill with the old request id did nothing.
+        self.assertEqual(last_header['action'], ACTION_REQUEST_SUCCESS)
+
+        # The termination message has i_image == n_images
+        write_message = receiver.recv_json()
+        # self.assertEqual(write_message['i_image'], n_images)
+
+        sender.close()
+        receiver.close()
+
+        listener.stop()
+        client.stop()
