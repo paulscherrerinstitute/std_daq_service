aec069bd5ca860452258af55d6c75fccdf9f47f3
Andrej Babic <andrej.babic@psi.ch>
2021-06-23 11:07:11 +0200

    Adjusted write service test to new architecture

--- tests/test_request_write_service.py
+++ tests/test_request_write_service.py
@@ -1,13 +1,11 @@
 import unittest
-from threading import Thread
 from time import sleep
 import zmq
 
-from std_daq_service.broker import broker_config
 from std_daq_service.broker.client import BrokerClient
-from std_daq_service.broker.broker_worker import BrokerWorker
+from std_daq_service.broker.common import TEST_BROKER_URL, ACTION_REQUEST_START, ACTION_REQUEST_SUCCESS
+from std_daq_service.broker.service import BrokerService
 from std_daq_service.protocol import ImageMetadata
-from std_daq_service.writer_agent.zmq_transciever import ZmqTransciever
 from std_daq_service.writer_agent.service import RequestWriterService
 
 
@@ -17,6 +15,7 @@ class TestRequestWriteService(unittest.TestCase):
         input_stream_url = "tcp://127.0.0.1:7000"
         output_stream_url = "tcp://127.0.0.1:7001"
         service_name = "test_service"
+        tag = service_name
 
         request = {
             "n_images": 10,
@@ -32,38 +31,38 @@ class TestRequestWriteService(unittest.TestCase):
         receiver.connect(output_stream_url)
         receiver.setsockopt_string(zmq.SUBSCRIBE, "")
 
-        service = RequestWriterService()
+        service = RequestWriterService(input_stream_url=input_stream_url,
+                                       output_stream_url=output_stream_url)
 
-        transceiver = ZmqTransciever(input_stream_url=input_stream_url,
-                                     output_stream_url=output_stream_url,
-                                     on_message_function=service.on_stream_message)
+        listener = BrokerService(broker_url=TEST_BROKER_URL,
+                                 tag=tag,
+                                 service_name=service_name,
+                                 request_callback=service.on_request,
+                                 kill_callback=service.on_kill)
 
-        listener = BrokerWorker(broker_url=broker_config.TEST_BROKER_URL,
-                                name=service_name,
-                                request_tag=service_name,
-                                on_request_message_function=service.on_broker_message)
-        t_service = Thread(target=listener.start)
-        t_service.start()
-
-        def on_status_message(request_id, header, body):
+        def on_status_message(request_id, request, header):
             nonlocal last_header
             last_header = header
+
         last_header = None
 
-        client = BrokerClient(broker_url=broker_config.TEST_BROKER_URL,
-                              status_tag="*",
-                              on_status_message_function=on_status_message)
-        t_client = Thread(target=client.start)
-        t_client.start()
+        client = BrokerClient(broker_url=TEST_BROKER_URL,
+                              tag=tag,
+                              status_callback=on_status_message)
+        sleep(0.1)
+        client.send_request(request)
         sleep(0.1)
 
-        client.send_request(service_name, request)
-        sleep(0.1)
+        self.assertEqual(last_header['action'], ACTION_REQUEST_START)
 
-        self.assertEqual(last_header['action'], broker_config.ACTION_REQUEST_START)
+        version = 1
+        image_height = 1024
+        image_width = 512
+        dtype = 1
+        encoding = 2
 
         for pulse_id in range(request["n_images"]):
-            sender.send(ImageMetadata(pulse_id, 0, 0, 0))
+            sender.send(ImageMetadata(version, pulse_id, image_height, image_width, dtype, encoding))
 
         for pulse_id in range(request["n_images"]):
             write_message = receiver.recv_json()
@@ -71,17 +70,18 @@ class TestRequestWriteService(unittest.TestCase):
             self.assertEqual(write_message["i_image"], write_message["image_metadata"]["id"])
             self.assertEqual(write_message["i_image"], pulse_id)
             self.assertEqual(write_message["output_file"], request["output_file"])
-            # TODO: Test also image metadata.
+
+            self.assertEqual(write_message['image_metadata']["version"], version)
+            self.assertEqual(write_message['image_metadata']["height"], image_height)
+            self.assertEqual(write_message['image_metadata']["width"], image_width)
+            self.assertEqual(write_message['image_metadata']["dtype"], dtype)
+            self.assertEqual(write_message['image_metadata']["encoding"], encoding)
 
         sleep(0.1)
-        self.assertEqual(last_header['action'], broker_config.ACTION_REQUEST_SUCCESS)
-
-        listener.stop()
-        t_service.join()
-
-        client.stop()
-        t_client.join()
+        self.assertEqual(last_header['action'], ACTION_REQUEST_SUCCESS)
 
         sender.close()
-        transceiver.stop()
         receiver.close()
+
+        listener.stop()
+        client.stop()
