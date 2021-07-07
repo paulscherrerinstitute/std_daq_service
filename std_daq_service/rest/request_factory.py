ebcf61380f6ce0ab0c2a1d2d14b0eaa106bde8bb
Andrej Babic <andrej.babic@psi.ch>
2021-07-07 11:38:28 +0200

    Removed factory for kill request
    Already supported by broker client

--- std_daq_service/rest/request_factory.py
+++ std_daq_service/rest/request_factory.py
@@ -12,8 +12,3 @@ def build_write_request(output_file, n_images, sources):
 def build_broker_response(response):
     # TODO: Convert the response into something the user can read.
     return response
-
-
-def build_kill_request(request_id):
-    # TODO: Decide on how to format of the kill request.
-    return None
