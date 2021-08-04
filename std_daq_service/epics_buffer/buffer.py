255ea1a83d1ab2319b78ad1ed553cd9f3a6a1322
Andrej Babic <andrej.babic@psi.ch>
2021-08-04 16:09:12 +0200

    Add epics buffering mechanism

---
+++ std_daq_service/epics_buffer/buffer.py
@@ -0,0 +1,28 @@
+import time
+
+
+class EpicsBuffer(object):
+    def __init__(self, pv_names):
+        self.pv_names = pv_names
+        self.cache = {}
+
+        event_timestamp = time.time()
+
+        for pv_name in self.pv_names:
+            self.cache[pv_name] = {
+                'event_timestamp': event_timestamp,
+                'connected': False,
+                'value': None,
+                'value_timestamp': None,
+                'value_status': None
+            }
+
+    def change_callback(self, pv_name, event_timestamp, connected,
+                        value, value_timestamp, value_status):
+        self.cache[pv_name] = {
+            'event_timestamp': event_timestamp,
+            'connected': connected,
+            'value': value,
+            'value_timestamp': value_timestamp,
+            'value_status': value_status
+        }
