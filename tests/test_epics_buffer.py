5aacc117f56ad2b10d5e238e4d660224cbe20bc6
Andrej Babic <andrej.babic@psi.ch>
2021-08-05 12:35:11 +0200

    Fix buffer tests

--- tests/test_epics_buffer.py
+++ tests/test_epics_buffer.py
@@ -1,10 +1,13 @@
 import json
 import random
 import unittest
+from multiprocessing import Process
+from time import sleep
 
 import zmq
 from epics import CAProcess
 from pcaspy import Driver, SimpleServer
+
 from std_daq_service.epics_buffer.receiver import EpicsReceiver
 from std_daq_service.epics_buffer.start_receiver import start_epics_buffer
 
@@ -12,34 +15,18 @@ from std_daq_service.epics_buffer.start_receiver import start_epics_buffer
 class TestEpicsBuffer(unittest.TestCase):
     def test_events(self):
         pv_names = ["ioc:pv_1", "ioc:pv_2"]
-
         buffer = []
 
         def callback_function(**kwargs):
             buffer.append(kwargs)
 
+        ioc_process = Process(target=start_test_ioc)
+        ioc_process.start()
+
         EpicsReceiver(pv_names, callback_function)
 
-        class TestIoc(Driver):
-            prefix = "ioc:"
-            pvdb = {
-                "pv_1": {"scan": 1},
-                "pv_2": {"scan": 2}
-            }
-
-            def __init__(self):
-                Driver.__init__(self)
-
-            def read(self, reason):
-                value = random.random()
-                return value
-
-        ioc_server = SimpleServer()
-        ioc_server.createPV(TestIoc.prefix, TestIoc.pvdb)
-        driver = TestIoc()
-
         while len(buffer) < 5:
-            ioc_server.process(0.1)
+            sleep(0.1)
 
         # First 2 records come as connection updates.
         self.assertEqual({buffer[0]['pv_name'], buffer[1]['pv_name']}, set(pv_names))
@@ -53,38 +40,18 @@ class TestEpicsBuffer(unittest.TestCase):
             self.assertTrue(buffer[i]['value'] is not None)
         self.assertEqual(pv_updates, set(pv_names))
 
-        ioc_server.cl
+        ioc_process.terminate()
 
     def test_receiver(self):
         sampling_pv = 'ioc:pulse_id'
         pv_names = ['ioc:pv_1', 'ioc:pv_2']
         stream_url = 'tcp://127.0.0.1:7000'
+
+        ioc_process = Process(target=start_test_ioc)
+        ioc_process.start()
+
         recv_process = CAProcess(target=start_epics_buffer, args=(sampling_pv, pv_names, stream_url))
         recv_process.start()
-
-        class TestIoc(Driver):
-            prefix = "ioc:"
-            pvdb = {
-                "pulse_id": {"scan": 0.1},
-                "pv_1": {"scan": 1},
-                "pv_2": {"scan": 2}
-            }
-
-            def __init__(self):
-                Driver.__init__(self)
-                self.pulse_id = 0
-
-            def read(self, reason):
-                if reason == "pulse_id":
-                    self.pulse_id += 1
-                    return self.pulse_id
-
-                value = random.random()
-                return value
-
-        ioc_server = SimpleServer()
-        ioc_server.createPV(TestIoc.prefix, TestIoc.pvdb)
-        driver = TestIoc()
 
         ctx = zmq.Context()
         receiver = ctx.socket(zmq.SUB)
@@ -101,15 +68,47 @@ class TestEpicsBuffer(unittest.TestCase):
             except zmq.Again:
                 pass
 
-            ioc_server.process(0.01)
-
         receiver.close()
         recv_process.terminate()
+        ioc_process.terminate()
 
+        offset = 0
         for i in range(20):
+
+            if i == 0:
+                offset = recv_buffer[i][0]
+
             recv_pulse_id = recv_buffer[i][0]
             recv_data = json.loads(recv_buffer[i][1])
 
-            self.assertEqual(recv_pulse_id, i)
-            # self.assertEqual(recv_data["ioc:pv_1"]["event_timestamp"])
-            print(recv_data)
+            self.assertEqual(recv_pulse_id, i + offset)
+            # print(recv_pulse_id, recv_data)
+
+
+def start_test_ioc():
+    class TestIoc(Driver):
+        prefix = "ioc:"
+        pvdb = {
+            "pulse_id": {"scan": 0.1},
+            "pv_1": {"scan": 1},
+            "pv_2": {"scan": 2}
+        }
+
+        def __init__(self):
+            Driver.__init__(self)
+            self.pulse_id = 0
+
+        def read(self, reason):
+            if reason == "pulse_id":
+                self.pulse_id += 1
+                return self.pulse_id
+
+            value = random.random()
+            return value
+
+    ioc_server = SimpleServer()
+    ioc_server.createPV(TestIoc.prefix, TestIoc.pvdb)
+    TestIoc()
+
+    while True:
+        ioc_server.process(0.1)
