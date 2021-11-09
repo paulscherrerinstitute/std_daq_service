f040882e7b939a57d63087a494064b7cff02db99
Andrej Babic <andrej.babic@psi.ch>
2021-11-09 13:29:06 +0100

    Add stats recording to the buffer

--- std_daq_service/epics_buffer/buffer.py
+++ std_daq_service/epics_buffer/buffer.py
@@ -13,6 +13,8 @@ from std_daq_service.epics_buffer.stats import EpicsBufferStats
 REDIS_MAX_STREAM_LEN = 3600*24*10
 # 100/second updates for pulse_id channel.
 REDIS_PULSE_ID_MAX_STREAM_LEN = 3600*24*100
+# Name of the stream for pulse_id mapping.
+REDIS_PULSE_ID_STREAM_NAME = "pulse_id"
 # Interval for printing out statistics.
 STATS_INTERVAL = 10
 
@@ -26,6 +28,7 @@ def start_epics_buffer(service_name, redis_host, pv_names, pulse_id_pv=None):
     stats = EpicsBufferStats(service_name=service_name)
 
     def on_pv_change(pv_name, value):
+        stats.record(pv_name, value)
         redis.xadd(pv_name, value, maxlen=REDIS_MAX_STREAM_LEN)
     EpicsReceiver(pv_names=pv_names, change_callback=on_pv_change)
 
@@ -33,15 +36,19 @@ def start_epics_buffer(service_name, redis_host, pv_names, pulse_id_pv=None):
         _logger.info(f"Adding pulse_id_pv {pulse_id_pv} to buffer.")
 
         def on_pulse_id_change(_, value):
-            redis.xadd("pulse_id", value, maxlen=REDIS_PULSE_ID_MAX_STREAM_LEN)
+            stats.record(REDIS_PULSE_ID_STREAM_NAME, value)
+            redis.xadd(REDIS_PULSE_ID_STREAM_NAME, value, maxlen=REDIS_PULSE_ID_MAX_STREAM_LEN)
         EpicsReceiver(pv_names=[pulse_id_pv], change_callback=on_pulse_id_change)
 
     try:
         sleep(STATS_INTERVAL)
-
+        stats.write_stats()
 
     except KeyboardInterrupt:
         _logger.info("Received interrupt signal. Exiting.")
 
     except Exception:
         _logger.error("Epics buffer error.")
+
+    finally:
+        stats.close()
