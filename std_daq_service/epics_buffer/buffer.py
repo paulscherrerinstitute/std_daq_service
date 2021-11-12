3455519deb71f1673618ccef39811f8220a4f4b9
Andrej Babic <andrej.babic@psi.ch>
2021-11-12 12:00:51 +0100

    Improve buffer code by extracting redis method

--- std_daq_service/epics_buffer/buffer.py
+++ std_daq_service/epics_buffer/buffer.py
@@ -6,11 +6,10 @@ from redis import Redis
 import numpy as np
 
 from std_daq_service.epics_buffer.receiver import EpicsReceiver
+from std_daq_service.epics_buffer.stats import EpicsBufferStats
 
 # Redis buffer for 1 day.
 # max 10/second updates for regular channels.
-from std_daq_service.epics_buffer.stats import EpicsBufferStats
-
 REDIS_MAX_STREAM_LEN = 3600*24*10
 # 100/second updates for pulse_id channel.
 REDIS_PULSE_ID_MAX_STREAM_LEN = 3600*24*100
@@ -35,17 +34,21 @@ def start_epics_buffer(service_name, redis_host, pv_names, pulse_id_pv=None):
     redis = Redis(host=redis_host)
     stats = EpicsBufferStats(service_name=service_name)
 
+    def send_to_redis(stream_name, value):
+        # Convert dictionary to bytes to store in Redis.
+        raw_value = json.dumps(value, cls=RedisJsonSerializer).encode('utf-8')
+        redis.xadd(stream_name, raw_value, maxlen=REDIS_MAX_STREAM_LEN)
+        stats.record(stream_name, raw_value)
+
     def on_pv_change(pv_name, value):
-        stats.record(pv_name, value)
-        redis.xadd(pv_name, value, maxlen=REDIS_MAX_STREAM_LEN)
+        send_to_redis(pv_name, value)
     EpicsReceiver(pv_names=pv_names, change_callback=on_pv_change)
 
     if pulse_id_pv:
         _logger.info(f"Adding pulse_id_pv {pulse_id_pv} to buffer.")
 
         def on_pulse_id_change(_, value):
-            stats.record(REDIS_PULSE_ID_STREAM_NAME, value)
-            redis.xadd(REDIS_PULSE_ID_STREAM_NAME, value, maxlen=REDIS_PULSE_ID_MAX_STREAM_LEN)
+            send_to_redis(REDIS_PULSE_ID_STREAM_NAME, value)
         EpicsReceiver(pv_names=[pulse_id_pv], change_callback=on_pulse_id_change)
 
     try:
@@ -60,3 +63,4 @@ def start_epics_buffer(service_name, redis_host, pv_names, pulse_id_pv=None):
 
     finally:
         stats.close()
+
