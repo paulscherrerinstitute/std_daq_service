3b7547261dfab0076ae973b79d9264f9a818373b
Andrej Babic <andrej.babic@psi.ch>
2021-11-12 14:17:50 +0100

    Improve statistics module

--- std_daq_service/epics_buffer/stats.py
+++ std_daq_service/epics_buffer/stats.py
@@ -1,14 +1,17 @@
 import logging
-from time import time
+from time import time_ns
 
-_logger = logging.getLogger("EpicsIngestionStats")
+_logger = logging.getLogger("EpicsBufferStats")
 
-DEFAULT_OUTPUT_FILE = 'stats.log'
+DEFAULT_OUTPUT_FILE = '/var/log/%s.stats.log'
 
 
 class EpicsBufferStats (object):
-    def __init__(self, service_name, output_file=DEFAULT_OUTPUT_FILE):
+    def __init__(self, service_name, output_file=None):
         self.service_name = service_name
+
+        if output_file is None:
+            output_file = DEFAULT_OUTPUT_FILE % service_name
         self.output_file = output_file
         _logger.info("Starting service {service_name} stats logging to {self.output_file}.")
 
@@ -16,7 +19,7 @@ class EpicsBufferStats (object):
         self._reset_stats()
 
         self.output_file = open(self.output_file, 'a')
-        self.start_time = time()
+        self.start_time = time_ns()
 
     def _reset_stats(self):
         self.stats = {
@@ -24,15 +27,22 @@ class EpicsBufferStats (object):
             'n_bytes': 0,
         }
 
-    def record(self, pv_name, event_timestamp, connected,
-               value, value_timestamp, value_status, value_type):
+    def record(self, name, data):
         self.stats['n_events'] += 1
-        # TODO: Get the real size of the update.
-        self.stats['n_bytes'] += 1
+        self.stats['n_bytes'] += len(data)
 
     def write_stats(self):
-        # TODO: Prepare the correct output
-        stats_output = "SOMETHNG"
+        end_time = time_ns()
+        throughput = self.stats['n_bytes'] / (end_time - self.start_time) * 10**9
+
+        # InfluxDB line protocol
+        stats_output = f'epics_buffer,service_name={self.service_name}' \
+                       f' n_events={self.stats["n_events"]}i' \
+                       f' n_bytes={self.stats["n_bytes"]}i' \
+                       f' throughput={throughput}' \
+                       f' {end_time}'
+
+        self.start_time = end_time
         self._reset_stats()
 
         self.output_file.write(stats_output)
