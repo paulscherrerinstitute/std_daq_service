ec8a7d5b5d041ce58d1d2ad8a03a24306d6d4a9a
Andrej Babic <andrej.babic@psi.ch>
2021-11-26 10:13:23 +0100

    Fix TAG name in docker build

--- build_docker.sh
+++ build_docker.sh
@@ -3,7 +3,7 @@
 VERSION=1.0.1
 
 docker build --no-cache=true -f -t paulscherrerinstitute/std-daq-service .
-docker tag paulscherrerinstitute/std-daq-service-base paulscherrerinstitute/std-daq-service:$VERSION
+docker tag paulscherrerinstitute/std-daq-service paulscherrerinstitute/std-daq-service:$VERSION
 
 docker login
 docker push paulscherrerinstitute/std-daq-service:$VERSION
