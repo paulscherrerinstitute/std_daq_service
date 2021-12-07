2f54090856eaffe20293fd13c3e606e3bb898029
Andrej Babic <andrej.babic@psi.ch>
2021-12-07 11:26:36 +0100

    Remove example detector config from container

--- tests_in_docker.sh
+++ tests_in_docker.sh
@@ -4,8 +4,7 @@ set -e
 
 docker build -t unittest-std-daq-service:latest . &&
 docker run --rm --net=host \
-  --env PIPELINE_NAME=debug.test_pipeline \
-  --env INSTANCE_NAME=epics_buffer \
+  --env SERVICE_NAME=debug.test_pipeline.epics_buffer \
   --env REDIS_SKIP=1 \
  unittest-std-daq-service:latest \
  python -m unittest discover tests/
