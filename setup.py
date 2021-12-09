f433ef6822b2c37a5528cef0cad3266b914ca581
Andrej Babic <andrej.babic@psi.ch>
2021-12-09 15:49:40 +0100

    Strip file to bare

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@ from setuptools import setup
 
 setup(
     name='std_daq_service',
-    version='1.0',
-    py_modules=['std_daq_service'],
-    install_requires=[
-        'pyzmq',
-        'pyepics',
-        'flask',
-        'pika',
-        'redis',
-        'pcaspy'
-    ],
     entry_points='''
         [console_scripts]
         epics_buffer=std_daq_service.epics_buffer.start:main
