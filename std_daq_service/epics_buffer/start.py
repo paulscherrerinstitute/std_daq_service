e45a56cbfab96f4e0a886d7e64be1fc4617e3f38
Andrej Babic <andrej.babic@psi.ch>
2021-12-09 15:52:12 +0100

    Modify service start script

--- std_daq_service/epics_buffer/start.py
+++ std_daq_service/epics_buffer/start.py
@@ -5,7 +5,8 @@ from std_daq_service.start_utils import read_service_arguments
 
 _logger = logging.getLogger("EpicsBuffer")
 
-if __name__ == "__main__":
+
+def main():
     service_name, config, redis_host = read_service_arguments("Epics buffer service")
 
     _logger.info(f'Service {service_name} starting.')
@@ -23,3 +24,7 @@ if __name__ == "__main__":
                        pulse_id_pv=pulse_id_pv)
 
     _logger.info(f'Service {service_name} stopping.')
+
+
+if __name__ == "__main__":
+    main()
