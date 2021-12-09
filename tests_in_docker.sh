d29f5620ea97c4f7a440d17326bb9e149d5c1f0c
Andrej Babic <andrej.babic@psi.ch>
2021-12-09 14:19:49 +0100

    Improve test script

--- tests_in_docker.sh
+++ tests_in_docker.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-docker build -t unittest-std-daq-service:latest . &&
+docker build -t unittest-std-daq-service:latest .
+
 docker run --rm --net=host \
   --env SERVICE_NAME=debug.test_pipeline.epics_buffer \
   --env REDIS_SKIP=1 \
- unittest-std-daq-service:latest \
- python -m unittest discover tests/
+    unittest-std-daq-service:latest \
+    python -m unittest discover tests/
