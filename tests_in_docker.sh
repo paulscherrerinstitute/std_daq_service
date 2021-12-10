1306bb643b33806539f5d4685819f8525bd23828
Andrej Babic <andrej.babic@psi.ch>
2021-12-10 15:20:13 +0100

    Skip entrypoint for testing

--- tests_in_docker.sh
+++ tests_in_docker.sh
@@ -5,7 +5,6 @@ set -e
 docker build -t unittest-std-daq-service:latest .
 
 docker run --rm --net=host \
-  --env SERVICE_NAME=debug.test_pipeline.epics_buffer \
-  --env REDIS_SKIP=1 \
+    --entrypoint "" \
     unittest-std-daq-service:latest \
     python -m unittest discover tests/
