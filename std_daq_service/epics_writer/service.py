b724f9ca9b9caca479bf361bc55e010e3924f5ce
Andrej Babic <andrej.babic@psi.ch>
2021-12-14 13:34:24 +0100

    Finalized the writer service

--- std_daq_service/epics_writer/service.py
+++ std_daq_service/epics_writer/service.py
@@ -1,28 +1,113 @@
+import json
 from logging import getLogger
-from threading import Event
 
-import zmq
+from redis import Redis
 
-from std_daq_service.protocol import ImageMetadata
+from std_daq_service.epics_writer.writer import EpicsH5Writer
 
 _logger = getLogger("EpicsWriterService")
 
+# Max 10 seconds pulse_id mismatch.
+MAX_PULSE_ID_MISMATCH = 1000
+
+
+def extract_request(request):
+    try:
+        start_pulse_id = request["start_pulse_id"]
+        stop_pulse_id = request['stop_pulse_id']
+
+        pv_list = request['channels']
+        output_file = request['output_file']
+        metadata = request.get('metadata', None)
+    except KeyError as e:
+        raise Exception('Invalid request from broker. Missing required fields.') from e
+
+    _logger.info(f"Write request from {start_pulse_id} to {stop_pulse_id} into {output_file}.")
+    _logger.debug(f"Metadata: {metadata}")
+    _logger.debug(f"Requested channels: {pv_list}")
+
+    return start_pulse_id, stop_pulse_id, pv_list, output_file, metadata
+
+
+def map_pulse_id_to_timestamp_range(redis, start_pulse_id, stop_pulse_id):
+    _logger.debug(f"Mapping from {start_pulse_id} to {stop_pulse_id}.")
+
+    def read_timestamp(response, original_pulse_id):
+        if not response:
+            raise RuntimeError(f"Cannot map {original_pulse_id} to timestamp.")
+
+        # Response in format [(b'pulse_id-0', {b'timestamp': b'1639480220636463612'})]
+        received_pulse_id = int(response[0][0].decode().split('-')[0])
+        timestamp = int(response[0][1]["timestamp".encode()].decode())
+
+        if abs(original_pulse_id - received_pulse_id) > MAX_PULSE_ID_MISMATCH:
+            raise RuntimeError(f"Received pulse_id {received_pulse_id} "
+                               f"too far away from requested pulse_id {original_pulse_id}.")
+
+        return timestamp
+
+    start_timestamp = read_timestamp(response=redis.xrevrange("pulse_id", max=start_pulse_id, count=1),
+                                     original_pulse_id=start_pulse_id)
+    stop_timestamp = read_timestamp(response=redis.xrange("pulse_id", min=stop_pulse_id, count=1),
+                                    original_pulse_id=stop_pulse_id)
+
+    _logger.debug("Mapped to range from {start_timestamp} to {stop_timestamp}.")
+
+    return start_timestamp, stop_timestamp
+
+
+def download_pv_data(redis, pv, start_timestamp, stop_timestamp):
+    _logger.debug(f'Downloading PV {pv} data from {start_timestamp} to {stop_timestamp}.')
+    data = []
+
+    # First data point: either exactly on the timestamp or first from the past.
+    data.extend(redis.xrevrange(pv, max=start_timestamp, count=1))
+    # Range in between the first data point and the last data point (exclusive).
+    data.extend(redis.xrange(pv, min=f'({start_timestamp}', max=f'({stop_timestamp}-0'))
+    # Last data point: either exactly on the timestamp of next in the future.
+    data.extend(redis.xrange(pv, min=stop_timestamp, count=1))
+
+    _logger.info(f"Downloaded {len(data)} data points for PV {pv}.")
+
+    # Response in format [(b'event_timestamp-0', {b'json': b'JSON_STRING'})]
+    return [json.loads(x[1]['json'.encode()].decode()) for x in data]
+
 
 class EpicsWriterService(object):
-    def __init__(self, buffer_folder):
-        _logger.info(f"Starting on epics buffer folder {buffer_folder}.")
+    def __init__(self, redis_host):
+        self.redis_host = redis_host
 
-        self.buffer_folder = buffer_folder
+        self._cancel_request = None
+        self._current_request = None
 
     def on_request(self, request_id, request):
-        self.interrupt_request.clear()
-        self.current_request_id = request_id
+        # We take into account only kill requests that come after the start of the current request.
+        self._cancel_request = None
+        self._current_request = request_id
 
-        n_images = request['n_images']
+        start_pulse_id, stop_pulse_id, pv_list, output_file, metadata = extract_request(request)
 
-        _logger.info(f"Starting write request for n_images {writer_stream_data['n_images']} "
-                     f"in {writer_stream_data['output_file']}")
+        writer = EpicsH5Writer(output_file=output_file)
+        redis = Redis(host=self.redis_host)
+
+        try:
+            if metadata:
+                writer.write_metadata(metadata)
+
+            start_timestamp, stop_timestamp = map_pulse_id_to_timestamp_range(redis, start_pulse_id, stop_pulse_id)
+
+            for pv_name in pv_list:
+                try:
+                    pv_data = download_pv_data(redis, pv_name, start_timestamp, stop_timestamp)
+                    writer.write_pv(pv_name, pv_data)
+                except Exception as e:
+                    _logger.exception(f"Error while writing PV {pv_name}.")
+
+                if self._cancel_request == self._current_request:
+                    raise Exception("User requested interruption.")
+        finally:
+            writer.close()
+            redis.close()
 
     def on_kill(self, request_id):
-        if self.current_request_id == request_id:
-            self.interrupt_request.set()
+        self._cancel_request = request_id
