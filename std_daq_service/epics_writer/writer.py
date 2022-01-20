7ff9cf2f70a3126968c0da0d8b41c6c05cb4b2c5
Andrej Babic <andrej.babic@psi.ch>
2022-01-20 11:23:44 +0100

    Implement data conversion method to numpy

--- std_daq_service/epics_writer/writer.py
+++ std_daq_service/epics_writer/writer.py
@@ -1,8 +1,62 @@
 import logging
+import numpy as np
 import os
+import struct
+from collections import Counter
+
 import h5py
 
 _logger = logging.getLogger("EpicsWriterFile")
+
+
+def prepare_data_for_writing(pv_name, pv_data):
+    if not pv_data:
+        _logger.warning(f"PV data for {pv_name} is empty.")
+        return
+
+    n_data_points = len(pv_data)
+
+    dtypes_count = Counter(value[b'type'] for _, value in pv_data)
+    if len(dtypes_count) > 1:
+        _logger.warning(f'Multiple data types for {pv_name}: {dtypes_count}')
+        return
+    dtype = dtypes_count.most_common(1)[0][0].decode()
+    dataset_type = dtype
+    if dataset_type == 'string':
+        dataset_type = object
+
+    shape_count = Counter(value[b'shape'] for _, value in pv_data)
+    if len(shape_count) > 1:
+        _logger.warning(f'Multiple data shapes for {pv_name}: {dtypes_count}')
+        return
+    shape_bytes = shape_count.most_common(1)[0][0]
+    dshape = struct.unpack(f"<{len(shape_bytes) // 4}I", shape_bytes)
+    dataset_shape = [n_data_points] + list(dshape)
+
+    dataset_value = np.zeros(shape=dataset_shape, dtype=dataset_type)
+    dataset_timestamp = np.zeros(shape=[n_data_points, 1], dtype='<f8')
+    dataset_status = np.zeros(shape=[n_data_points, 1], dtype=object)
+    dataset_connected = np.zeros(shape=[n_data_points, 1], dtype='<u1')
+
+    for index, data_point in enumerate(pv_data):
+        redis_id, value = data_point
+
+        timestamp = float(value[b'id'].decode())
+        dataset_timestamp[index] = timestamp
+
+        if dtype == 'string':
+            data_point_value = value[b'value'].decode()
+        else:
+            data_point_value = np.frombuffer(value[b'value'], dtype=dtype).reshape(dshape)
+        dataset_value[index] = data_point_value
+
+        connected = int(value[b'connected'])
+        dataset_connected[index] = connected
+
+        status = value[b'status'].decode()
+        dataset_status[index] = status
+
+    return n_data_points, dtype, dataset_timestamp, dataset_value, dataset_connected, dataset_status
 
 
 class EpicsH5Writer(object):
@@ -35,12 +89,10 @@ class EpicsH5Writer(object):
 
     def write_pv(self, pv_name, pv_data):
 
-        if not pv_data:
-            _logger.warning(f"PV data for {pv_name} is empty.")
-            return
+        n_data_points, dtype, dataset_timestamp, dataset_value, dataset_connected, dataset_status = \
+            prepare_data_for_writing(pv_name, pv_data)
 
-        # All the data points have the same fields - defined in the epics buffer.
-        fields = pv_data[0].keys()
-
-        for field in fields:
-            self.file.create_dataset(f'{pv_name}/{field}', data=[x[field] for x in pv_data])
+        self.file.create_dataset(f'{pv_name}/timestamp', data=dataset_timestamp)
+        self.file.create_dataset(f'{pv_name}/value', data=dataset_value)
+        self.file.create_dataset(f'{pv_name}/connected', data=dataset_connected)
+        self.file.create_dataset(f'{pv_name}/status', data=dataset_status)
