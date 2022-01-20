3e4e6ce416b9052b4f32558ffa5e7b44f44c5532
Andrej Babic <andrej.babic@psi.ch>
2022-01-20 12:24:09 +0100

    Services listen to all requests by default

--- std_daq_service/broker/primary_service.py
+++ std_daq_service/broker/primary_service.py
@@ -12,7 +12,7 @@ _logger = getLogger("BrokerService")
 
 class PrimaryBrokerService(BrokerClientBase):
     def __init__(self, broker_url, service_name, request_callback=None, kill_callback=None):
-        super().__init__(broker_url=broker_url, tag=service_name)
+        super().__init__(broker_url=broker_url, tag="#")
 
         self.service_name = service_name
         _logger.info(f"Service {service_name} starting.")
