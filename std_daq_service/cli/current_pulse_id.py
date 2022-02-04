c07d0563ee064de518b09b35dff332d7f5b139ea
Andrej Babic <andrej.babic@psi.ch>
2022-02-04 13:16:43 +0100

    Include port in the redis current pulse_id

--- std_daq_service/cli/current_pulse_id.py
+++ std_daq_service/cli/current_pulse_id.py
@@ -8,10 +8,12 @@ def main():
     parser = argparse.ArgumentParser(description='Read current pulse_id.')
     parser.add_argument("--redis_host", type=str, help="Host of Redis instance.",
                         default=os.environ.get("REDIS_HOST", '127.0.0.1'))
+    parser.add_argument("--redis_port", type=int, help='Port of redis instance',
+                        default=os.environ.get("REDIS_PORT", 6379))
     parser.add_argument("--channel", type=str, help="Channel with the pulse_id mapping", default="pulse_id")
 
     args = parser.parse_args()
-    redis = Redis(host=args.redis_host)
+    redis = Redis(host=args.redis_host, port=args.redis_port)
 
     response = redis.xrevrange("pulse_id", max="+", count=1)
     received_pulse_id = int(response[0][0].decode().split('-')[0])
