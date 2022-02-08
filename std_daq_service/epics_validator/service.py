974ae8b71b91f7b3d9fbfd3cda7fa80213c96d79
Andrej Babic <andrej.babic@psi.ch>
2022-02-08 14:43:53 +0100

    Implement on success writer

--- std_daq_service/epics_validator/service.py
+++ std_daq_service/epics_validator/service.py
@@ -1,19 +1,22 @@
+import time
 from logging import getLogger
 
 import h5py
+
+from std_daq_service.broker.common import ACTION_REQUEST_START, ACTION_REQUEST_SUCCESS, ACTION_REQUEST_FAIL
 
 _logger = getLogger("EpicsValidationService")
 
 
 class EpicsValidationService(object):
+    def __init__(self):
+        self.requests = {}
+        self.headers = {}
 
-    def on_request(self, request_id, request):
-        input_filename = request["output_file"]
+    def validate_file(self, request_id):
         _logger.info(f"Validating request {request_id} on file {input_filename}.")
-
-        expected_channels = request["channels"]
-
-        with h5py.File(input_filename, mode='r') as input_file:
+        output_file = self.requests[request_id]['output_file']
+        with h5py.File(output_file, mode='r') as input_file:
 
             missing_channels = []
             for channel in expected_channels:
@@ -27,5 +30,46 @@ class EpicsValidationService(object):
             "missing_channels": missing_channels
         }
 
-    def on_kill(self, request_id):
+    def print_run_log(self, request_id, message):
+        run_log_file = self.requests[request_id].get("run_log_file")
+        if run_log_file is None:
+            return
+
+
+    def on_request_start(self, request_id, source, output_file):
+        start_pulse_id = self.requests[request_id]['start_pulse_id']
+        stop_pulse_id = self.requests[request_id]['stop_pulse_id']
+        n_channels = len(self.requests[request_id]['channels'])
+
+        output_text = f'[EPICS] Processing of {request_id} started in service {source}.\n'
+        output_text +=  f'[EPICS] Requesting file {output_file} for pulse_id range {start_pulse_id} to {stop_pulse_id}'\
+                        f'with {n_channels} channels.'
+
+        self.print_run_log(request_id=request_id, message=output_text)
+
+
+    def on_request_success(self, request_id):
+
+    def on_request_fail(self, request_id):
         pass
+
+    def on_status_change(self, request_id, request, header):
+
+        if request_id not in self.requests:
+            self.requests[request_id] = request
+            self.headers[request_id] = []
+
+        source = header['source']
+        header_entry = (time.time(), header, source)
+        self.headers[request_id].append(header_entry)
+
+        action = header['action']
+        if action == ACTION_REQUEST_START:
+            output_file = request['output_file']
+            self.on_request_start(request_id, source, output_file)
+
+        elif action == ACTION_REQUEST_SUCCESS:
+            self.on_request_success(request_id)
+
+        elif action == ACTION_REQUEST_FAIL:
+            self.on_request_fail(request_id)
