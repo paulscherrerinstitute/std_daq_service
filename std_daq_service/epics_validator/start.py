0643cc1eec9f0c3ed8097c4103ae433f4facd8e7
Andrej Babic <andrej.babic@ringlspil.com>
2022-05-11 16:24:07 +0200

    Add Dima functionality

--- std_daq_service/epics_validator/start.py
+++ std_daq_service/epics_validator/start.py
@@ -13,17 +13,43 @@ _logger = logging.getLogger("EpicsWriter")
 
 def validate_file(request_id, request):
     output_file = request['output_file']
+    start_pulse_id = request["start_pulse_id"]
+    stop_pulse_id = request['stop_pulse_id']
+
     _logger.info(f"Validating request {request_id} file {output_file}.")
 
     expected_channels = request['channels']
     with h5py.File(output_file, mode='r') as input_file:
 
-        missing_channels = []
+        validation_data = []
         for channel in expected_channels:
             if channel not in input_file:
-                missing_channels.append(f"Missing pv {channel}")
+                validation_data.append(f"[{channel}] Missing data")
+                continue
 
-    return missing_channels
+            pulse_id_map = input_file[channel].get('pulse_id', None)
+            if pulse_id_map is None:
+                validation_data.append(f'[{channel}] Missing pulse_id mapping')
+            else:
+                n_pulses_before_start = 0
+                for pulse_id in pulse_id_map:
+                    if pulse_id < start_pulse_id:
+                        n_pulses_before_start += 1
+                    else:
+                        break
+
+                n_pulses_after_end = 0
+                for pulse_id in pulse_id_map[::-1]:
+                    if pulse_id >= stop_pulse_id:
+                        n_pulses_after_end += 1
+                    else:
+                        break
+
+                n_pulses_in_interval = len(pulse_id_map) - n_pulses_before_start - n_pulses_after_end
+                validation_data.append(f'[{channel}] Point count {len(pulse_id_map)} '
+                                       f'({n_pulses_before_start}[{n_pulses_in_interval}){n_pulses_after_end})')
+
+    return validation_data
 
 
 def main():
