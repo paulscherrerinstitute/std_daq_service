43e6f3722807572379d8a8092ba3f8f6410cb4b7
Leonardo Hax Damiani <lhdamiani@gmail.com>
2022-12-09 15:52:42 +0100

    error msg improvements on rest interface

--- std_daq_service/rest/start_gf.py
+++ std_daq_service/rest/start_gf.py
@@ -15,47 +15,95 @@ from std_daq_service.start_utils import default_service_setup
 _logger = logging.getLogger("RestProxyService")
 
 
+class NoTraceBackWithLineNumber(Exception):
+    def __init__(self, msg):
+        if type(msg).__name__ in ["ConnectionError", "ReadTimeout"]:
+            print(
+                "\n ConnectionError/ReadTimeout: it seems that the server "
+                "is not running (check xbl-daq-32 pco writer service "
+                "(pco_writer-pco{1-2}), ports, etc).\n"
+            )
+        try:
+            ln = sys.exc_info()[-1].tb_lineno
+        except AttributeError:
+            ln = inspect.currentframe().f_back.f_lineno
+        self.args = (
+            "{0.__name__} (line {1}): {2}".format(type(self), ln, msg),
+        )
+        sys.tracebacklimit = None
+        return None
+
+
+class StdDaqAPIError(NoTraceBackWithLineNumber):
+    pass
+
+
 def start_rest_api(service_name, broker_url, tag, config_file):
     app = Flask(service_name)
     manager = RestManager(broker_url=broker_url, tag=tag)
 
+    @app.route("/alive", methods=["GET"])
+    def alive():
+        return jsonify({"status": 200, "alive": "Yes"})
+
     @app.route("/write_sync", methods=["POST"])
     def write_sync_request():
-        message = extract_write_request(request.json)
+        error_msg = ""
+        try:
+            message = extract_write_request(request.json)
+        except Exception as e:
+            error_msg = str(e)
 
-        request_id, broker_response = manager.write_sync(message)
+        response_dict = {}
 
-        return jsonify(
-            {
+        if error_msg == "":
+            request_id, broker_response = manager.write_sync(message)
+
+            response_dict = {
                 "request_id": request_id,
                 "response": build_user_response(response=broker_response),
             }
-        )
+
+        if error_msg != "":
+            response_dict["error"] = error_msg
+        return jsonify(response_dict)
 
     @app.route("/write_async", methods=["POST"])
     def write_async_request():
-        message = extract_write_request(request.json)
+        response_dict = {}
+        try:
+            message = extract_write_request(request.json)
+        except Exception as e:
+            error_msg = str(e)
 
-        request_id = manager.write_async(message)
+        if error_msg == "":
+            request_id = manager.write_async(message)
+            response_dict = {"request_id": request_id}
+        else:
+            response_dict["error"] = error_msg
 
-        return jsonify({"request_id": request_id})
+        return jsonify(response_dict)
 
     @app.route("/write_kill", methods=["POST"])
     def write_kill():
         kill_request = request.json
-
+        response_dict = {}
+        error_msg = ""
         if "request_id" not in kill_request:
-            raise RuntimeError('Mandatory field "request_id" missing.')
-        request_id = kill_request["request_id"]
-
-        broker_response = manager.kill_sync(request_id)
-
-        return jsonify(
-            {
-                "request_id": request_id,
-                "response": build_user_response(response=broker_response),
-            }
-        )
+            response_dict["error"] = 'Mandatory field "request_id" missing.'
+        else:
+            request_id = kill_request["request_id"]
+            try:
+                broker_response = manager.kill_sync(request_id)
+            except Exception as e:
+                error_msg = str(e)
+                response_dict["error"] = error_msg
+            if error_msg == "":
+                response_dict = {
+                    "request_id": request_id,
+                    "response": build_user_response(response=broker_response),
+                }
+        return jsonify(response_dict)
 
     app.run(host="0.0.0.0", port=5000)
 
