961111eebc5a4877f09778410435814a453ba3f4
babic_a <andrej.babic@psi.ch>
2023-03-02 09:53:29 +0100

    Add pycaspy requirement

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name='std_daq_service',
+    install_requires=[
+        'pcaspy'
+    ],
     entry_points='''
         [console_scripts]
         epics_buffer=std_daq_service.epics_buffer.start:main
