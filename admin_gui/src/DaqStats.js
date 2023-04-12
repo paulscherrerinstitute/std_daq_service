6eb367e186806ca038e015a123bb8fe805fb0bf4
babic_a <andrej.babic@psi.ch>
2023-04-12 16:13:26 +0200

    Add DAQ statistics for detector

--- admin_gui/src/DaqStats.js
+++ admin_gui/src/DaqStats.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import LaunchIcon from '@material-ui/icons/Launch';
 import SettingsIcon from '@material-ui/icons/Settings'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -13,20 +13,37 @@ import {
     TextField,
     Button
 } from '@mui/material';
+import axios from "axios";
 
-function DaqStats(props) {
-  const { state } = props;
+function DaqStats() {
+  const [ state, setState ] = useState({bytes_per_second: 0, images_per_second: 0});
 
-  const n_bytes = 0;
-  const n_images = 0;
-  const stats_url = "http://www.google.com";
+  const n_mbytes = state.bytes_per_second / 1024 / 1024;
+  const bandwidth_text = `${n_mbytes.toFixed(2)} MB/s`;
+
+  const n_images = state.images_per_second;
+  const frequency_text = `${n_images.toFixed(2)} Hz`;
+  const stats_url = "http://localhost:5000/stats";
 
   let status_chip;
-  if (n_bytes > 0) {
+  if (n_mbytes > 0) {
      status_chip = <Chip variant="contained" label="Streaming" color="success" />;
   } else {
       status_chip = <Chip variant="contained" label="Idle" color="info" />;
   }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(stats_url);
+      setState(result.data.stats);
+    };
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Paper sx={{ p: 2 }} elevation={3}>
@@ -39,11 +56,11 @@ function DaqStats(props) {
 
       <Grid container alignItems="center" spacing={1}>
         <Grid item> <Typography variant="subtitle2">Bandwidth:</Typography></Grid>
-        <Grid item> {n_bytes/1024/1024} MB/s </Grid>
+        <Grid item> {bandwidth_text} </Grid>
       </Grid>
       <Grid container alignItems="center" spacing={1}>
         <Grid item> <Typography variant="subtitle2">Frequency:</Typography></Grid>
-        <Grid item> {n_images} Hz </Grid>
+        <Grid item> {frequency_text} </Grid>
       </Grid>
 
       <a href={stats_url} target="_blank">
