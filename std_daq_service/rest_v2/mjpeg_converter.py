1281a6f8b3930ef7b3a10626e856b7fbcf485aed
Andrej Babic <andrej.babic@psi.ch>
2023-04-26 16:12:04 +0200

    Improve mjpeg_converter

--- std_daq_service/rest_v2/mjpeg_converter.py
+++ std_daq_service/rest_v2/mjpeg_converter.py
@@ -34,45 +34,57 @@ def generate_frames():
     receiver.setsockopt_string(zmq.SUBSCRIBE, "")
     receiver.setsockopt(zmq.RCVTIMEO, RECV_TIMEOUT)
 
-    frame = np.zeros(shape=(WIDTH, HEIGHT), dtype=np.uint8)
+    frame = None
+    image_id = None
+    full_circle = True
+
     while True:
         try:
             raw_meta, raw_data = receiver.recv_multipart()
             meta = json.loads(raw_meta.decode('utf-8'))
-            image_id = meta["frame"]
-            frame = np.frombuffer(raw_data, dtype=meta['type']).reshape(meta['shape'])
+            new_frame = np.frombuffer(raw_data, dtype=meta['type']).reshape(meta['shape'])
 
-            frame = cv2.resize(frame, (WIDTH, HEIGHT))
-            frame = cv2.flip(frame, 0)
+            # Flip the image vertically.
+            new_frame = cv2.flip(new_frame, 0)
 
             # Scale image to 8 bits with full range.
-            min_val = frame.min()
-            max_val = frame.max()
-            frame = ((frame - min_val) * (255.0 / (max_val - min_val))).clip(0, 255).astype(np.uint8)
+            min_val = new_frame.min()
+            max_val = new_frame.max() or 1
+
+            frame = ((new_frame - min_val) * (255.0 / (max_val - min_val))).clip(0, 255).astype(np.uint8)
+            image_id = meta["frame"]
 
         except Again:
+            frame = np.zeros(shape=(HEIGHT, WIDTH), dtype=np.uint8)
             image_id = None
-
+        
+        image = cv2.resize(frame, (WIDTH, HEIGHT))
         # apply a color scheme to the grayscale image
-        color_frame = cv2.applyColorMap(frame, cv2.COLORMAP_HOT)
+        image = cv2.applyColorMap(image, cv2.COLORMAP_HOT)
+
+
 
         font = cv2.FONT_HERSHEY_SIMPLEX
+        # Valid image.
         if image_id is not None:
             text = 'Frame {}'.format(image_id)
             text_color = (0, 255, 0)
         else:
-            text = 'No data...'
+            text = 'No data'
             text_color = (0, 0, 255)
+            # Display circle next to text to show connection to the mjpeg stream.
+            image = cv2.circle(image,  (160, HEIGHT - 40), 8, (0, 0, 255), -1 if full_circle else 2)
+            full_circle = not full_circle
 
         text_size = cv2.getTextSize(text, font, 1, 2)[0]
-        cv2.putText(color_frame, text, (10, HEIGHT - text_size[1] - 10), font, 1, text_color, 2)
+        cv2.putText(image, text, (10, HEIGHT - text_size[1] - 10), font, 1, text_color, 2)
 
         # encode the color image as JPEG
-        _, buffer = cv2.imencode('.jpg', color_frame)
-        frame = buffer.tobytes()
+        _, buffer = cv2.imencode('.jpg', image)
+        image_bytes = buffer.tobytes()
 
         # yield the frame for the MJPG stream
-        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + image_bytes + b'\r\n'
 
     receiver.close()
 
