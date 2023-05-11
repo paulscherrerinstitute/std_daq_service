3ad4cd2679e5e35a005779dfc2cf977775b657fa
babic_a <andrej.babic@psi.ch>
2023-05-12 00:08:15 +0200

    Interface rework for tune_network

--- std_daq_service/tools/tune_network.py
+++ std_daq_service/tools/tune_network.py
@@ -37,35 +37,37 @@ def get_module_id_to_irq_map(interface):
     return {i: x[0][:-1] for i, x in enumerate(lines)}
 
 
-def tune(machine_config, interfaces, start_udp_port):
-    _logger.info(f'Tuning interfaces {interfaces} with start_udp_port {start_udp_port}.')
+def tune(machine_config, interface, start_udp_port):
+    _logger.info(f'Tuning interface {interface} with start_udp_port {start_udp_port}.')
 
     map_module_to_cpu = get_module_id_to_cpu_id_map(machine_config=machine_config)
-    for interface in interfaces:
-        map_module_id_to_irq = get_module_id_to_irq_map(interface)
+    map_module_id_to_irq = get_module_id_to_irq_map(interface)
 
-        cmd = f"ethtool -u {interface} | grep Filter: | awk '{{print $2}}' | xargs -L1 ethtool -U {interface} delete"
-        subprocess.run(cmd, shell=True, check=True)
+    cmd = f"ethtool -u {interface} | grep Filter: | awk '{{print $2}}' | xargs -L1 ethtool -U {interface} delete"
+    subprocess.run(cmd, shell=True)
 
-        for module_id, cpu_id in map_module_to_cpu.items():
-            irq = map_module_id_to_irq[module_id]
-            irq_file = f"/proc/irq/{irq}/smp_affinity_list"
+    for module_id, cpu_id in map_module_to_cpu.items():
+        irq = map_module_id_to_irq[module_id]
+        irq_file = f"/proc/irq/{irq}/smp_affinity_list"
 
-            with open(irq_file, 'w') as output_file:
-                output_file.write(cpu_id)
+        with open(irq_file, 'w') as output_file:
+            output_file.write(cpu_id)
 
-            udp_port = start_udp_port + module_id
-            cmd = ["ethtool", "-U", interface, "flow-type", "udp4", "dst-port", udp_port, "action", module_id]
-            subprocess.run(cmd, check=True)
+        udp_port = int(start_udp_port) + module_id
+        cmd = ["ethtool", "-U", interface, "flow-type", "udp4", "dst-port", str(udp_port), "action", str(module_id)]
+        subprocess.run(cmd, check=True)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Tune network on receiver machine')
     parser.add_argument("deployment_config", type=str, help="Path to the site.yaml deployment config.")
     parser.add_argument('daq_config', type=str, help='Path to daq config file.')
+    parser.add_argument('network_interface', type=str, help='Name of the network interface to tune.')
 
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
+
+    network_interface = args.network_interface
 
     with open(args.deployment_config, 'r') as input_file:
         deployment_config = yaml.safe_load(input_file)
@@ -78,7 +80,7 @@ def main():
         _logger.info(f'Inspecting config hostname {config_hostname}.')
 
         if is_hostname_from_this_machine(machine_config['hosts']):
-            tune(machine_config, ['ens2f', 'ens5f'], int(daq_config['start_udp_port']))
+            tune(machine_config, network_interface, daq_config['start_udp_port'])
             break
     else:
         _logger.error("No hostname matches the current machine.")
