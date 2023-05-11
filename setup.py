13e52857f2415d3d6e32eeb0ec0e9f5d0a0f6e85
babic_a <andrej.babic@psi.ch>
2023-05-12 00:09:20 +0200

    Tool has moved, fix setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         std_daq_current_pulse_id=std_daq_service.tools.current_pulse_id:main
         
         std_daq_rest=std_daq_service.rest_v2.start:main
-        std_daq_config_deployer=std_daq_service.config_deployer.start:main
+        std_daq_config_deployer=std_daq_service.tools.config_deployer:main
         std_daq_tune_network=std_daq_service.tools.tune_network:main
         std_daq_udp_simulator=std_daq_service.udp_simulator.start_rest:main
         std_daq_mjpeg_stream=std_daq_service.rest_v2.mjpeg:main
