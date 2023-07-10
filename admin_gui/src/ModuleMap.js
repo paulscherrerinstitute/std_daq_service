f8125bd0b9d27da76542562d09c3f67eb9210ed1
babic_a <andrej.babic@psi.ch>
2023-07-10 23:09:30 +0200

    Extend file image to return module_map

---
+++ admin_gui/src/ModuleMap.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Box, Dialog, DialogTitle, IconButton, Typography, LinearProgress } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box display="flex" alignItems="center">
+      <Box width="100%" mr={1}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box minWidth={35}>
+        <Typography variant="body2" color="text.primary">{`${Math.round(
+          props.value,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default function ModuleMapDialog({ open, handleClose, filename, i_image }) {
+  const [buffer, setBuffer] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (open) {
+      axios({
+        method: 'get',
+        url: 'http://localhost:5000/file/1/1?module_map=1',
+        responseType: 'arraybuffer',
+        onDownloadProgress: function (progressEvent) {
+          setProgress((progressEvent.loaded / progressEvent.total) * 100);
+        },
+      }).then(function (response) {
+        let blob = new Blob([response.data], { type: response.headers['content-type'] });
+        let url = URL.createObjectURL(blob);
+        setBuffer(url);
+        setLoaded(true);
+      });
+    }
+  }, [open]);
+
+  return (
+    <Dialog open={open} fullScreen>
+      <DialogTitle>
+        <b>Module map </b> filename={filename} i_image={i_image}
+        <IconButton edge="end" color="inherit" onClick={handleClose} aria-label="close">
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      {!loaded ?
+        <LinearProgressWithLabel value={progress} />
+        :
+        <img src={buffer} style={{ objectFit: "contain", maxHeight: "calc(100% - 128px)", maxWidth: "100%" }} alt="Module Map" />
+      }
+    </Dialog>
+  );
+}
--- std_daq_service/rest_v2/rest.py
+++ std_daq_service/rest_v2/rest.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from logging import getLogger
+from typing import Optional
 
 import cv2
 import numpy as np
@@ -32,11 +33,22 @@ DAQ_LOGS_ENDPOINT = '/daq/logs/{n_logs}'
 DAQ_DEPLOYMENT_STATUS_ENDPOINT = '/daq/deployment'
 
 FILE_METADATA = '/file/{acquisition_id}'
-FILE_IMAGE = '/file/{acquisition_id}/{i_image}'
+FILE_IMAGE = '/fileview/{acquisition_id}/{i_image}'
 
 request_logger = getLogger('request_log')
 _logger = getLogger('rest')
 
+
+def get_font_scale(target_height_in_pixels: int):
+    # Set a base scale
+    scale = 1.0
+    # Use a typical text
+    text = "Test text"
+    # Get the size of the text box
+    ((text_width, text_height), _) = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, scale, 1)
+    # Calculate the target font scale
+    target_font_scale = target_height_in_pixels / text_height
+    return target_font_scale
 
 def read_file_metadata(output_file):
     return {
@@ -66,19 +78,92 @@ def register_rest_interface(app, writer_manager: WriterRestManager, daq_manager:
                     "file_info": read_file_metadata(log['info']['output_file'])
                 }}
 
-    @app.route(FILE_IMAGE)
-    def get_file_image(acquisition_id: int, i_image: int):
-        STREAM_WIDTH = 640
-        STREAM_HEIGHT = 480
-        frame = np.random.randint(low=0, high=256, size=(STREAM_HEIGHT, STREAM_WIDTH), dtype=np.uint8)
-        image = cv2.resize(frame, (STREAM_WIDTH, STREAM_HEIGHT))
-        # apply a color scheme to the grayscale image
+    @app.get('/file/{acquisition_id}/{i_image}')
+    def get_file_image(acquisition_id: int, i_image: int, module_map: Optional[int] = 0, gaps: Optional[int] = 0):
+        _, daq_config = daq_manager.get_config()
+        image_pixel_width = daq_config['image_pixel_width']
+        image_pixel_height = daq_config['image_pixel_height']
+
+        rectangle_color = (0, 255, 0)  # Green
+        rectangle_thickness = 2  # Pixels
+        text_color = (0, 255, 0)  # Green
+
+        frame = np.random.randint(low=0, high=256, size=(image_pixel_height, image_pixel_width), dtype=np.uint8)
+
+        image = cv2.resize(frame, (image_pixel_width, image_pixel_height))
         image = cv2.applyColorMap(image, cv2.COLORMAP_HOT)
 
-        _, buffer = cv2.imencode('.jpg', image)
+        if module_map == 1:
+            for key, (x_start, y_start, x_end, y_end) in daq_config['module_positions'].items():
+                # Draw the rectangle
+                cv2.rectangle(image, (x_start, y_start), (x_end, y_end), rectangle_color, rectangle_thickness)
+
+                # Draw the text for the module and its submodules
+                text_start_y = y_start if y_start < y_end else y_end
+                size_y = abs(y_end - y_start) + 20
+                gap_y = size_y // 5
+                font_scale = get_font_scale(30)
+                y_smaller = y_start if y_start < y_end else y_end
+                y_bigger = y_end if y_end > y_start else y_start
+
+                submodule_info = next((item for item in daq_config['submodule_info'] if item["submodule"] == int(key)), None)
+                if submodule_info:
+                    cv2.putText(image, f"module_id: {key}", (x_start + rectangle_thickness + 20,
+                                                             text_start_y + (1*gap_y)),
+                                cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, rectangle_thickness)
+                    cv2.putText(image, f"hostname: {submodule_info['hostname']}",
+                                (x_start + rectangle_thickness + 20, text_start_y + (2*gap_y)),
+                                cv2.FONT_HERSHEY_SIMPLEX, font_scale,
+                                text_color, rectangle_thickness)
+                    cv2.putText(image, f"port: {submodule_info['port']}",
+                                (x_start + rectangle_thickness + 20, text_start_y + (3*gap_y)),
+                                cv2.FONT_HERSHEY_SIMPLEX, font_scale,
+                                text_color, rectangle_thickness)
+                    cv2.putText(image, f"row: {submodule_info['row']}, column: {submodule_info['column']}",
+                        (x_start + rectangle_thickness + 20, text_start_y + (4 * gap_y)), cv2.FONT_HERSHEY_SIMPLEX,
+                        font_scale,
+                        text_color, rectangle_thickness)
+
+                arrow_color = (0, 255, 0)  # Red
+                arrow_thickness = 20  # Pixels
+                arrow_line_length = 100  # Length of the arrow's line
+                arrow_tip_length = 30  # Length of the two lines that make the arrow tip
+
+                arrow_start_x = x_end - 35  # adjust these as needed
+
+                if y_start > y_end:  # if true, arrow points upwards
+                    arrow_start_y = y_smaller + arrow_line_length + 20  # adjust these as needed
+                    arrow_end_y = arrow_start_y - arrow_line_length
+                    # Draw the arrow line
+                    cv2.line(image, (arrow_start_x, arrow_start_y), (arrow_start_x, arrow_end_y), arrow_color,
+                             arrow_thickness)
+                    # Draw the arrow tip
+                    cv2.line(image, (arrow_start_x, arrow_end_y),
+                             (arrow_start_x - arrow_tip_length // 2, arrow_end_y + arrow_tip_length), arrow_color,
+                             arrow_thickness)
+                    cv2.line(image, (arrow_start_x, arrow_end_y),
+                             (arrow_start_x + arrow_tip_length // 2, arrow_end_y + arrow_tip_length), arrow_color,
+                             arrow_thickness)
+                else:  # arrow points downwards
+                    arrow_start_y = y_bigger - 120  # adjust these as needed
+                    arrow_end_y = arrow_start_y + arrow_line_length
+                    # Draw the arrow line
+                    cv2.line(image, (arrow_start_x, arrow_start_y), (arrow_start_x, arrow_end_y), arrow_color,
+                             arrow_thickness)
+                    # Draw the arrow tip
+                    cv2.line(image, (arrow_start_x, arrow_end_y),
+                             (arrow_start_x - arrow_tip_length // 2, arrow_end_y - arrow_tip_length), arrow_color,
+                             arrow_thickness)
+                    cv2.line(image, (arrow_start_x, arrow_end_y),
+                             (arrow_start_x + arrow_tip_length // 2, arrow_end_y - arrow_tip_length), arrow_color,
+                             arrow_thickness)
+
+        _, buffer = cv2.imencode('.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, 50])
         image_bytes = buffer.tobytes()
 
-        return StreamingResponse(BytesIO(image_bytes), media_type='image/jpeg')
+        img_io = BytesIO(image_bytes)
+
+        return StreamingResponse(img_io, media_type='image/jpeg', headers={'Content-Length': str(len(image_bytes))})
 
     @app.post(WRITER_WRITE_SYNC_ENDPOINT, response_model=WriterResponse)
     def write_sync_request(write_request: WriteRequest):
@@ -148,7 +233,7 @@ def register_rest_interface(app, writer_manager: WriterRestManager, daq_manager:
     def get_daq_live_stream_request():
         return StreamingResponse(streamer.generate_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
 
-    @app.get(DAQ_CONFIG_ENDPOINT, response_model=ConfigResponse)
+    @app.get(DAQ_CONFIG_ENDPOINT)
     def get_daq_config_request():
         config_id, daq_config = daq_manager.get_config()
 
