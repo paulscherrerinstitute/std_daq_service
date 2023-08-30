fd608de5cb2aa37b818ce42c1cfd83d661b10b36
babic_a <andrej.babic@psi.ch>
2023-08-30 20:46:40 +0200

    Implement output buffer copy

--- std_daq_service/compression/start.py
+++ std_daq_service/compression/start.py
@@ -1,10 +1,9 @@
 import argparse
 import logging
-from time import sleep, time
+from time import time
 
 import bitshuffle
-import numpy as np
-from std_buffer.image_metadata_pb2 import ImageMetadata
+from std_buffer.image_metadata_pb2 import ImageMetadata, ImageMetadataStatus
 import zmq
 from zmq import Again
 
@@ -24,6 +23,7 @@ def start_compression(config_file):
     block_size = 0
 
     image_metadata_address = f"ipc:///tmp/{detector_name}-image"
+    compressed_metadata_address = f"ipc:///tmp/{detector_name}-compressed"
 
     # Receive the image metadata stream from the detector.
     ctx = zmq.Context()
@@ -32,8 +32,13 @@ def start_compression(config_file):
     image_metadata_receiver.setsockopt_string(zmq.SUBSCRIBE, "")
     image_metadata_receiver.setsockopt(zmq.RCVTIMEO, 200)
 
-    buffer = RamBuffer(channel_name=detector_name, shape=shape, dtype=dtype,
-                       data_n_bytes=image_n_bytes, n_slots=1000)
+    image_metadata_sender = ctx.socket(zmq.PUB)
+    image_metadata_sender.bind(compressed_metadata_address)
+
+    input_buffer = RamBuffer(channel_name=detector_name, shape=shape, dtype=dtype,
+                             data_n_bytes=image_n_bytes, n_slots=1000)
+    output_buffer = RamBuffer(channel_name=detector_name, shape=shape, dtype=dtype,
+                              data_n_bytes=image_n_bytes, n_slots=1000)
 
     image_meta = ImageMetadata()
     compressed_bytes = 0
@@ -45,21 +50,28 @@ def start_compression(config_file):
             meta_raw = image_metadata_receiver.recv()
             image_meta.ParseFromString(meta_raw)
 
-            data = buffer.get_data(image_meta.image_id)
-            uncompressed_bytes += data.nbytes
-
+            # Compress data into output buffer.
+            data = input_buffer.get_data(image_meta.image_id)
             compressed_data = bitshuffle.compress_lz4(data, block_size)
-            compressed_bytes += compressed_data.nbytes
+            output_buffer.write(image_meta.image_id, compressed_data)
 
+            # Update ImageMetadata header
+            image_meta.size = compressed_data.nbytes
+            image_meta.status = ImageMetadataStatus.compressed_image
+            image_metadata_sender.send(image_meta.SerializeToString())
+
+            # Update statistics.
+            uncompressed_bytes += data.nbytes
+            compressed_bytes += compressed_data.nbytes
             n_compressions += 1
 
             end_time = time()
             if end_time - start_time > 1:
                 start_time = end_time
 
-                print(f'Uncompressed {uncompressed_bytes/1024/1024} MB/s; '
-                      f'Compressed {compressed_bytes/1024/1024} MB/s; '
-                      f'Ratio {compressed_bytes/uncompressed_bytes}; '
+                print(f'Uncompressed {uncompressed_bytes / 1024 / 1024} MB/s; '
+                      f'Compressed {compressed_bytes / 1024 / 1024} MB/s; '
+                      f'Ratio {compressed_bytes / uncompressed_bytes}; '
                       f'Frequency {n_compressions} Hz')
                 uncompressed_bytes = 0
                 compressed_bytes = 0
